411abd10e7bf5efb6855a5ede7875950a87a71d5
GutsyMean <timothy.bogun@gmail.com>
2023-04-19 15:55:43 -0400

    trying to live

--- src/circleCatcher/CircleCatcher.java
+++ src/circleCatcher/CircleCatcher.java
@@ -1,10 +1,25 @@
 package circleCatcher;
 
-public class CircleCatcher {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class CircleCatcher extends Application
+{
+	public void start(Stage primaryStage)  
+    {
+    	
+    	
+    	
+    	
+    	primaryStage.setTitle("Circle Catcher");
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(400);
+        primaryStage.setWidth(600);
+        primaryStage.show();
+    }
+	public static void main(String[] args) 
+	{
+		launch(args);
 	}
 
 }
--- src/circleCatcher/RandomCircle.java
+++ src/circleCatcher/RandomCircle.java
@@ -1,10 +1,40 @@
 package circleCatcher;
 
-public class RandomCircle {
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
+public class RandomCircle extends Circle
+{
+	private int radius;
+	private int centerX;
+	private int centerY;
+	private int randRed;
+	private int randGreen;
+	private int randBlue;
+	
+	Random rand = new Random();
+	Circle circle = new Circle();
+	Color color = new Color(randRed, randGreen, randBlue, 1.0);
+
+	
+	public RandomCircle()
+	{
+		radius = rand.nextInt(20, 200);
+		centerX = rand.nextInt(600);
+		centerY = rand.nextInt(400);
+		
+		randRed = rand.nextInt(254);
+		randGreen = rand.nextInt(254);
+		randBlue = rand.nextInt(254);
+		
+		
+		circle = new Circle(centerX, centerY, radius, color);	
+		
 	}
-
+	public Circle getRandomCircle()
+	{
+		return circle;
+	}
 }
