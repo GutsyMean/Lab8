a3cbfa15195f5f85eb2aec4f247084b25784ab27
GutsyMean <timothy.bogun@gmail.com>
2023-04-24 15:46:39 -0400

    good stuff

--- src/circleCatcher/CircleCatcher.java
+++ src/circleCatcher/CircleCatcher.java
@@ -1,18 +1,28 @@
 package circleCatcher;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class CircleCatcher extends Application
 {
+
 	private static final int WINDOW_WIDTH = 600;
 	private static final int WINDOW_HEIGHT = 400;
 	private static final int NUM_CIRCLES = 10;
 	
+	@Override
 	public void start(Stage primaryStage)  
-    {
+    {	    
     	Pane pane = new Pane();
     	for (int i = 0; i < NUM_CIRCLES; i++)
     	{
@@ -23,11 +33,16 @@ public class CircleCatcher extends Application
 
     	primaryStage.setTitle("Circle Catcher");
         primaryStage.setScene(scene);
+//        primaryStage.setMinWidth(scene.getWidth());
+//        System.out.println(scene.getWidth());
+//        System.out.println(scene.getHeight());
+//
+//        primaryStage.setMinHeight(scene.getHeight());
+
 //        primaryStage.setWidth(WINDOW_WIDTH);
 //        primaryStage.setHeight(WINDOW_HEIGHT);
         primaryStage.show();
     }
-	
 	public static void main(String[] args) 
 	{
 		launch(args);
--- src/circleCatcher/RandomCircle.java
+++ src/circleCatcher/RandomCircle.java
@@ -25,59 +25,98 @@ public class RandomCircle extends Circle
     
 	static Random rand = new Random();
 	Circle circle = new Circle();
+	TranslateTransition move = new TranslateTransition();
 	
 	public RandomCircle(int windowWidth, int windowHeight)
 	{
 		super(rand.nextInt(MIN_RADIUS,MAX_RADIUS));
 //		this.windowWidth = windowWidth;
 //		this.windowHeight = windowHeight;
-		
+		move = new TranslateTransition(Duration.seconds(3), RandomCircle.this);
 		setFill(getRandomColor());
-		setLayoutX(rand.nextInt(300));
-		setLayoutY(rand.nextInt(200));
-		initAnimation(windowWidth, windowHeight);
-		initMousePressedHandler((windowWidth / 2), (windowHeight / 2));
+		move.setFromX(rand.nextDouble(getRadius(), windowWidth-getRadius()));
+		move.setFromY(rand.nextDouble(getRadius(), windowHeight-getRadius()));
+		move.setToX(rand.nextDouble(getRadius(), windowWidth-getRadius()));
+		move.setToY(rand.nextDouble(getRadius(), windowHeight-getRadius()));
+		move.setCycleCount(TranslateTransition.INDEFINITE);
+		move.setAutoReverse(true);
+		move.play();
+		//setCenterX(rand.nextDouble(getRadius(), windowWidth-getRadius()));
+		//setCenterY(rand.nextDouble(getRadius(), windowHeight-getRadius()));
+		
+		//initAnimation(windowWidth, windowHeight);
+
+		setOnMousePressed(event ->
+		{
+			//animation.stop();
+//          Path path = new Path();
+//          path.getElements().add(new MoveTo(startX, startY));
+//          path.getElements().add(new LineTo(centerX, centerY));
+//          PathTransition transition = new PathTransition(Duration.seconds(1), path, RandomCircle.this);
+//          transition.play();
+         
+
+          //TranslateTransition moveToCenter = new TranslateTransition(Duration.seconds(1), RandomCircle.this);
+          //moveToCenter.setToX((windowWidth/2));
+          move.stop();
+		  move.setFromX(event.getSceneX());
+          move.setFromY(event.getSceneY());
+		  move.setToX(300);
+          move.setToY(200);
+          //moveToCenter.setToY((windowHeight/2));
+          System.out.println(getCenterX());
+          System.out.println(getCenterY());
+          move.setAutoReverse(false);
+          move.setCycleCount(1);
+//          System.out.println(windowWidth/2);
+
+          move.play();
+		});
+
+//		initMousePressedHandler((windowWidth / 2), (windowHeight / 2));
 		
 	}
 	
 	private void initAnimation(int windowWidth, int windowHeight) 
 	{
-		startX = getLayoutX();
-		startY = getLayoutY();
-	    endX = rand.nextDouble(300);
-	    endY = rand.nextDouble(200);
+		//startX = move.getCenterX();
+		startY = getCenterY();
+	    endX = rand.nextDouble(getRadius(), windowWidth-getRadius());
+	    endY = rand.nextDouble(getRadius(), windowHeight-getRadius());
+	    
         Path path = new Path();
         path.getElements().add(new MoveTo(startX, startY));
         path.getElements().add(new LineTo(endX, endY));
         path.getElements().add(new LineTo(startX, startY));
-        animation = new PathTransition(Duration.seconds(rand.nextInt(1,3)), path, this);
+        animation = new PathTransition(Duration.seconds(rand.nextInt(4,5)), path, this);
         animation.setCycleCount(PathTransition.INDEFINITE);
         animation.play();
+        
     }
 
-    private void initMousePressedHandler(int centerX, int centerY) 
-    {
-        setOnMousePressed(new EventHandler<MouseEvent>() 
-        {
-            @Override
-            public void handle(MouseEvent event) 
-            {
-                animation.stop();
-//                Path path = new Path();
-//                path.getElements().add(new MoveTo(startX, startY));
-//                path.getElements().add(new LineTo(centerX, centerY));
-//                PathTransition transition = new PathTransition(Duration.seconds(1), path, RandomCircle.this);
-//                transition.play();
-               
-
-                TranslateTransition moveToCenter = new TranslateTransition(Duration.seconds(1), RandomCircle.this);
-                moveToCenter.setToX(centerX - getCenterX());
-                moveToCenter.setToY(centerY - getCenterY());
-
-                moveToCenter.play();
-            }
-        });
-    }
+//    private void initMousePressedHandler(int centerX, int centerY) 
+//    {
+//        setOnMousePressed(new EventHandler<MouseEvent>() 
+//        {
+//            @Override
+//            public void handle(MouseEvent event) 
+//            {
+//                animation.stop();
+////                Path path = new Path();
+////                path.getElements().add(new MoveTo(startX, startY));
+////                path.getElements().add(new LineTo(centerX, centerY));
+////                PathTransition transition = new PathTransition(Duration.seconds(1), path, RandomCircle.this);
+////                transition.play();
+//               
+//
+//                TranslateTransition moveToCenter = new TranslateTransition(Duration.seconds(1), RandomCircle.this);
+//                moveToCenter.setToX(centerX - getCenterX());
+//                moveToCenter.setToY(centerY - getCenterY());
+//
+//                moveToCenter.play();
+//            }
+//        });
+//    }
     private Color getRandomColor() 
     {
 
