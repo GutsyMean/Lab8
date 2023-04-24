98ca3cac897668cb517fd59d6286ca944ce70e7e
GutsyMean <timothy.bogun@gmail.com>
2023-04-24 11:40:22 -0400

    update 1

--- src/circleCatcher/CircleCatcher.java
+++ src/circleCatcher/CircleCatcher.java
@@ -1,22 +1,31 @@
 package circleCatcher;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class CircleCatcher extends Application
 {
+	private static final int WINDOW_WIDTH = 600;
+	private static final int WINDOW_HEIGHT = 400;
+	private static final int NUM_CIRCLES = 10;
+	
 	public void start(Stage primaryStage)  
     {
-    	
-    	
-    	
-    	
+    	Pane pane = new Pane();
+    	for (int i = 0; i < NUM_CIRCLES; i++)
+    	{
+    		RandomCircle circle = new RandomCircle(WINDOW_WIDTH, WINDOW_HEIGHT);
+    		pane.getChildren().add(circle);
+    	}
+    	Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
+
     	primaryStage.setTitle("Circle Catcher");
         primaryStage.setScene(scene);
-        primaryStage.setHeight(400);
-        primaryStage.setWidth(600);
         primaryStage.show();
     }
+	
 	public static void main(String[] args) 
 	{
 		launch(args);
--- src/circleCatcher/RandomCircle.java
+++ src/circleCatcher/RandomCircle.java
@@ -1,40 +1,86 @@
 package circleCatcher;
 
 import java.util.Random;
-
+import javafx.animation.*;
+import javafx.event.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.*;
+import javafx.util.*;
 
 public class RandomCircle extends Circle
 {
-	private int radius;
-	private int centerX;
-	private int centerY;
-	private int randRed;
-	private int randGreen;
-	private int randBlue;
-	
-	Random rand = new Random();
-	Circle circle = new Circle();
-	Color color = new Color(randRed, randGreen, randBlue, 1.0);
 
+	private static final int MAX_RADIUS = 50;
+    private static final int MIN_RADIUS = 10;
+    
+    private PathTransition animation;
+
+    private double startX = getLayoutX();
+    private double startY = getLayoutY();
+    private double windowWidth;
+    private double windowHeight;
+    private double endX = Math.random() * (windowWidth - (2 * getRadius()));
+    private double endY = Math.random() * (windowHeight - (2 * getRadius()));
+    
+	static Random rand = new Random();
+	Circle circle = new Circle();
 	
-	public RandomCircle()
+	public RandomCircle(int windowWidth, int windowHeight)
 	{
-		radius = rand.nextInt(20, 200);
-		centerX = rand.nextInt(600);
-		centerY = rand.nextInt(400);
+		super(rand.nextInt(MIN_RADIUS,MAX_RADIUS));
+
 		
-		randRed = rand.nextInt(254);
-		randGreen = rand.nextInt(254);
-		randBlue = rand.nextInt(254);
+		setFill(getRandomColor());
+		setLayoutX(rand.nextInt(windowWidth-MAX_RADIUS)+MAX_RADIUS);
+		setLayoutY(rand.nextInt(windowHeight- MAX_RADIUS)+MAX_RADIUS);
+		initAnimation(windowWidth, windowHeight);
+		initMousePressedHandler((windowWidth / 2), (windowHeight / 2));
 		
-		
-		circle = new Circle(centerX, centerY, radius, color);	
 		
 	}
-	public Circle getRandomCircle()
+	
+	private void initAnimation(int windowWidth, int windowHeight) 
 	{
-		return circle;
-	}
+       
+        Path path = new Path();
+        path.getElements().add(new MoveTo(startX, startY));
+        path.getElements().add(new LineTo(endX, endY));
+        path.getElements().add(new LineTo(startX, startY));
+        animation = new PathTransition(Duration.seconds(rand.nextInt(1,3)), path, this);
+        animation.setCycleCount(PathTransition.INDEFINITE);
+        animation.play();
+    }
+
+    private void initMousePressedHandler(int centerX, int centerY) 
+    {
+        setOnMousePressed(new EventHandler<MouseEvent>() 
+        {
+            @Override
+            public void handle(MouseEvent event) 
+            {
+                animation.stop();
+//                Path path = new Path();
+//                path.getElements().add(new MoveTo(startX, startY));
+//                path.getElements().add(new LineTo(centerX, centerY));
+//                PathTransition transition = new PathTransition(Duration.seconds(1), path, RandomCircle.this);
+//                transition.play();
+               
+
+                TranslateTransition moveToCenter = new TranslateTransition(Duration.seconds(1), RandomCircle.this);
+                moveToCenter.setToX(centerX - getCenterX());
+                moveToCenter.setToY(centerY - getCenterY());
+
+                moveToCenter.play();
+            }
+        });
+    }
+    private Color getRandomColor() 
+    {
+        double red = Math.random();
+        double green = Math.random();
+        double blue = Math.random();
+        return new Color(red, green, blue, 1.0);
+    }
+    
 }
